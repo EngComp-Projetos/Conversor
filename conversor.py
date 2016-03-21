c7ee9f2724801e8053c5d98b5e1a8e0bdc2a0da2
Edilson Santiago <edilson.engcomp@gmail.com>
2016-03-21 09:32:02 -0300

    conversor

---
+++ conversor.py
@@ -0,0 +1,43 @@
+# coding=UTF-8
+__author__ = 'Edilson'
+import matplotlib.pyplot as plt
+import numpy as np
+
+amplitude = 2  # Amplitude do sinal
+frequencia = 2  # Frequência do sinal
+tempo_duracao = 1  # Tempo  de duração do sinal
+frequencia_angular = frequencia * 2 * np.pi  # Frequência angular
+tempo = np.linspace(0, tempo_duracao, 2000)
+sinal = amplitude * np.cos(frequencia_angular * tempo) ** 2
+numero_bits = 16
+frequencia_amostragem = 8 * frequencia
+periodo_amostragem = 1 / frequencia_amostragem
+
+quantizacao_bits = [amplitude / ((2 ** numero_bits) - 1)]
+for i in range(1, numero_bits):
+	quantizacao_bits.append(quantizacao_bits[i - 1] * 2)
+
+
+sinal_amostrado = []
+tempo_amostra = []
+i = 0
+while (i <= tempo_duracao):
+	
+	amostra = amplitude * np.cos(frequencia_angular * i) ** 2
+	soma = 0
+	aux = 0
+	for n in range(numero_bits - 1, 0, -1):
+		aux += quantizacao_bits[n]
+		if aux >= amostra:
+			aux = soma
+		else:
+			soma = aux
+	sinal_amostrado.append(soma) 
+	tempo_amostra.append(i)
+	i += periodo_amostragem
+
+plt.subplot(1,1,1)
+plt.plot(tempo, sinal)
+plt.subplot(1,1,1) 
+plt.plot(tempo_amostra, sinal_amostrado, drawstyle='steps-post')
+plt.show()
